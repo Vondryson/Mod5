55de94af301cfa94bb68c2a5725f377da623205a
Vondráček Jan <47273190+Vondryson@users.noreply.github.com>
2019-03-11 09:09:42 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -84,7 +84,7 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   function(responseText){
-	buildAndShowHomeHtml(responseText)
+	buildAndShowHomeHTML(responseText)
 
       }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
